7ff3f690e4ec6ae347ac4667262a806107129a33
Sao-Moni <keomony074@gmail.com>
2026-04-30 08:53:45 +0700

    testing

--- python.py
+++ python.py
@@ -1,3 +1,5 @@
+name = input("What is your name? ")
+print(f"Hello, {name}! Welcome to the binary to decimal converter.")
 binary = input("Enter the binary number: ")
 decimal = 0
 power = 0
@@ -5,3 +7,4 @@ for digit in binary[::-1]:
     decimal += int(digit) * (2 ** power)
     power += 1
 print(f"The decimal equivalent of {binary} is: {decimal}")
+print("thank you for using the binary to decimal convertor!")
